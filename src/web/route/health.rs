0dee61423fe4fd156a161db68b33be545eb2405e
Daniel Choi <choi@ibm.com>
2024-06-02 10:56:37 -0400

    htmx addition

--- src/web/route/health.rs
+++ src/web/route/health.rs
@@ -2,7 +2,7 @@ use actix_web::{
     get,
     http::{header::ContentType, StatusCode},
     web::{self, Data},
-    HttpResponse,
+    HttpMessage, HttpRequest, HttpResponse,
 };
 use tera::{Context, Tera};
 use tracing::instrument;
@@ -10,12 +10,29 @@ use tracing::instrument;
 #[get("")]
 #[instrument]
 async fn pulse(data: Data<Tera>) -> HttpResponse {
-    let bod = data.render("404.html", &Context::new()).unwrap();
+    let bod = data.render("pulse.html", &Context::new()).unwrap();
     HttpResponse::build(StatusCode::OK)
         .content_type(ContentType::html())
         .body(bod)
 }
 
+#[get("/status")]
+#[instrument]
+async fn status(req: HttpRequest) -> HttpResponse {
+    req.headers().get("hx-request").map_or_else(
+        || HttpResponse::BadRequest().finish(),
+        |val| {
+            if val == "true" {
+                HttpResponse::Ok()
+                    .content_type(ContentType::form_url_encoded())
+                    .body("<p>OK</p>")
+            } else {
+                HttpResponse::BadRequest().finish()
+            }
+        },
+    )
+}
+
 pub fn config_status(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/pulse").service(pulse));
+    cfg.service(web::scope("/pulse").service(pulse).service(status));
 }
