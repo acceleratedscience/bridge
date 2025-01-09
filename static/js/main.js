cf59328652d52fe1910ad6dcd343adc3ff2d8199
Daniel Choi <choi@ibm.com>
2025-01-08 22:30:27 -0500

    ui impl 3

--- static/js/main.js
+++ static/js/main.js
@@ -4,24 +4,26 @@ class Menu {
     menu;
     menu_open;
     menu_close;
-    constructor() {
-        this.menu_button = document.getElementById("menu_button");
-        this.menu_big = document.getElementById("menu_big").childNodes;
-        this.menu = document.getElementById("menu");
-        this.menu_open = document.getElementById("menu_open");
-        this.menu_close = document.getElementById("menu_close");
+    constructor(menu_labels) {
+        this.menu_button = document.getElementById(menu_labels.button);
+        this.menu_big = document.getElementById(menu_labels.big).childNodes;
+        this.menu = document.getElementById(menu_labels.menu);
+        this.menu_open = document.getElementById(menu_labels.open);
+        this.menu_close = document.getElementById(menu_labels.close);
+        // Menu item selection styling
         this.menu_big.forEach((node) => {
             node.addEventListener("click", () => {
                 this.menu_big.forEach((node) => {
                     if (node instanceof HTMLElement) {
-                        node.classList.remove("menu_selected");
+                        node.classList.remove(menu_labels.menu_selected);
                     }
                 });
                 if (node instanceof HTMLElement) {
-                    node.classList.add("menu_selected");
+                    node.classList.add(menu_labels.menu_selected);
                 }
             });
         });
+        // Mobile menu
         this.menu_button.addEventListener("click", () => {
             if (this.menu.classList.contains("hidden")) {
                 this.menu.classList.add("flex");
@@ -36,6 +38,7 @@ class Menu {
                 this.menu_close.classList.add("hidden");
             }
         });
+        // Mobile when close when clicking outside the menu
         document.addEventListener("click", (e) => {
             if (e.target instanceof HTMLElement && !this.menu_button.contains(e.target) && this.menu.classList.contains("flex")) {
                 this.menu.classList.add("hidden");
@@ -47,5 +50,13 @@ class Menu {
     }
 }
 window.addEventListener("DOMContentLoaded", () => {
-    const menu = new Menu();
+    const menu_labels = {
+        button: "menu_button",
+        big: "menu_big",
+        menu: "menu",
+        open: "menu_open",
+        close: "menu_close",
+        menu_selected: "menu_selected"
+    };
+    const menu = new Menu(menu_labels);
 });
--- static/ts/main.ts
+++ static/ts/main.ts
@@ -1,3 +1,12 @@
+interface menuLabels {
+	button: string;
+	big: string,
+	menu: string;
+	open: string;
+	close: string;
+	menu_selected: string;
+}
+
 class Menu {
 	menu_button: HTMLElement;
 	menu_big: NodeListOf<ChildNode>;
@@ -5,26 +14,28 @@ class Menu {
 	menu_open: HTMLElement;
 	menu_close: HTMLElement;
 
-	constructor() {
-		this.menu_button = document.getElementById("menu_button");
-		this.menu_big = document.getElementById("menu_big").childNodes;
-		this.menu = document.getElementById("menu");
-		this.menu_open = document.getElementById("menu_open");
-		this.menu_close = document.getElementById("menu_close");
+	constructor(menu_labels: menuLabels) {
+		this.menu_button = document.getElementById(menu_labels.button);
+		this.menu_big = document.getElementById(menu_labels.big).childNodes;
+		this.menu = document.getElementById(menu_labels.menu);
+		this.menu_open = document.getElementById(menu_labels.open);
+		this.menu_close = document.getElementById(menu_labels.close);
 
+		// Menu item selection styling
 		this.menu_big.forEach((node) => {
 			node.addEventListener("click", () => {
 				this.menu_big.forEach((node) => {
 					if (node instanceof HTMLElement) {
-						node.classList.remove("menu_selected");
+						node.classList.remove(menu_labels.menu_selected);
 					}
 				});
 				if (node instanceof HTMLElement) {
-					node.classList.add("menu_selected");
+					node.classList.add(menu_labels.menu_selected);
 				}
 			});
 		});
 
+		// Mobile menu
 		this.menu_button.addEventListener("click", () => {
 			if (this.menu.classList.contains("hidden")) {
 				this.menu.classList.add("flex");
@@ -39,6 +50,7 @@ class Menu {
 			}
 		});
 
+		// Mobile when close when clicking outside the menu
 		document.addEventListener("click", (e) => {
 			if (e.target instanceof HTMLElement && !this.menu_button.contains(e.target) && this.menu.classList.contains("flex")) {
 				this.menu.classList.add("hidden");
@@ -51,5 +63,15 @@ class Menu {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-	const menu: Menu = new Menu();
+
+	const menu_labels: menuLabels = {
+		button: "menu_button",
+		big: "menu_big",
+		menu: "menu",
+		open: "menu_open",
+		close: "menu_close",
+		menu_selected: "menu_selected"
+	};
+
+	const menu: Menu = new Menu(menu_labels);
 });
