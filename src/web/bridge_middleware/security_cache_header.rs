698af3a283544b9fe061a0a2b34cfe52fab09ec3
Daniel Choi <choi@ibm.com>
2025-02-18 19:36:09 -0500

    cache-control header for html

--- src/web/bridge_middleware/security_cache_header.rs
+++ src/web/bridge_middleware/security_cache_header.rs
@@ -2,14 +2,14 @@ use std::future::{ready, Ready};
 
 use actix_web::{
     dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
-    http::header::{self, HeaderValue},
+    http::header::{self, HeaderValue, CACHE_CONTROL},
     Error,
 };
 use futures::future::LocalBoxFuture;
 
-pub struct SecurityHeader;
+pub struct SecurityCacheHeader;
 
-impl<S, B> Transform<S, ServiceRequest> for SecurityHeader
+impl<S, B> Transform<S, ServiceRequest> for SecurityCacheHeader
 where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
     S::Future: 'static,
@@ -18,19 +18,19 @@ where
     type Response = ServiceResponse<B>;
     type Error = Error;
     type InitError = ();
-    type Transform = SecurityHeaderMW<S>;
+    type Transform = SecurityCacheHeaderMW<S>;
     type Future = Ready<Result<Self::Transform, Self::InitError>>;
 
     fn new_transform(&self, service: S) -> Self::Future {
-        ready(Ok(SecurityHeaderMW { service }))
+        ready(Ok(SecurityCacheHeaderMW { service }))
     }
 }
 
-pub struct SecurityHeaderMW<S> {
+pub struct SecurityCacheHeaderMW<S> {
     service: S,
 }
 
-impl<S, B> Service<ServiceRequest> for SecurityHeaderMW<S>
+impl<S, B> Service<ServiceRequest> for SecurityCacheHeaderMW<S>
 where
     S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error>,
     S::Future: 'static,
@@ -47,10 +47,12 @@ where
         let response = self.service.call(req);
         Box::pin(async move {
             let mut res = response.await?;
-            res.headers_mut().insert(
+            let header = res.headers_mut();
+            header.insert(
              header::CONTENT_SECURITY_POLICY,
              HeaderValue::from_str("default-src 'self'; font-src 1.www.s81c.com; img-src * data:; style-src 'self' 'unsafe-inline' https://1.www.s81c.com https://cdn.jsdelivr.net; script-src 'self' 'nonce-carbon-sucks' 'nonce-login-redirect-bridge' 'nonce-bridge-group-form-val' https://unpkg.com https://1.www.s81c.com https://cdn.jsdelivr.net;")?,
             );
+            header.insert(CACHE_CONTROL, HeaderValue::from_str("no-cache")?);
             Ok(res)
         })
     }
