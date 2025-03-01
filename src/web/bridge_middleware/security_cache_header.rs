7f4c888aa2440015fcc68d488c5d1c2ceb85d68b
Daniel Choi <choi@ibm.com>
2025-03-01 03:50:28 -0500

    security_cache_header

--- src/web/bridge_middleware/security_cache_header.rs
+++ src/web/bridge_middleware/security_cache_header.rs
@@ -50,7 +50,7 @@ where
             let header = res.headers_mut();
             header.insert(
              header::CONTENT_SECURITY_POLICY,
-             HeaderValue::from_str("default-src 'self'; font-src 1.www.s81c.com; img-src * data:; style-src 'self' 'unsafe-inline' https://1.www.s81c.com https://cdn.jsdelivr.net; script-src 'self' 'nonce-carbon-sucks' 'nonce-login-redirect-bridge' 'nonce-bridge-group-form-val' https://unpkg.com https://1.www.s81c.com https://cdn.jsdelivr.net;")?,
+             HeaderValue::from_str("default-src 'self' https://open.accelerate.science; font-src 1.www.s81c.com; img-src * data:; style-src 'self' 'unsafe-inline' https://1.www.s81c.com https://cdn.jsdelivr.net; script-src 'self' 'nonce-carbon-sucks' 'nonce-login-redirect-bridge' 'nonce-bridge-group-form-val' https://unpkg.com https://1.www.s81c.com https://cdn.jsdelivr.net;")?,
             );
             header.insert(CACHE_CONTROL, HeaderValue::from_str("no-cache")?);
             Ok(res)
