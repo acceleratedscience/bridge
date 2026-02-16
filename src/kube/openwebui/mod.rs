26f0b1f55ad0279ebe2c3cc5b935dd1e22c377b5
Daniel Choi <choi@ibm.com>
2026-02-16 09:32:41 -0500

    wip tomls

--- src/kube/openwebui/mod.rs
+++ src/kube/openwebui/mod.rs
@@ -6,38 +6,6 @@ use serde::{Deserialize, Serialize};
 
 pub const OWUI: &str = "owui";
 
-/*
-apiVersion: accelerate.science/v1
-kind: Owui
-metadata:
-  name: u67aceff11a66c1fa7c99726c-openwebui
-  namespace: openwebui
-spec:
-  # will remove this... makes no sense to have this
-  replica: 1
-  retainPVC: false
-
-  servicePort: 8080
-
-  image:
-    registry: quay.io
-    repository: ibmdpdev/open-webui-spati
-    tag: latest
-    pullPolicy: Always
-
-  persistence:
-    size: 2Gi
-    storageClass: gp3
-
-  env:
-    - name: MOLVIEWER_URL
-      value: "moleviewer.open.accelerate.science"
-    - name: WEBUI_SECRET_KEY
-      # i dunno just make one up
-      value: "idontcarewhatthisisbecuasethisisrequiredforsomereason"
- */
-
-// This is a placeholder for openwebui CRD
 #[derive(CustomResource, Clone, Deserialize, Serialize, Debug, JsonSchema)]
 #[kube(
     group = "accelerate.science",
@@ -80,45 +48,8 @@ struct Env {
 
 #[cfg(test)]
 mod test {
-    // use crate::config::CONFIG;
-    // use crate::kube::KubeAPI;
-
-    #[tokio::test]
-    async fn test_deserialize_owui() {
-        // let yaml_data = r#"
-        //     apiVersion: accelerate.science/v1
-        //     kind: Owui
-        //     metadata:
-        //       name: u67aceff11a66c1fa7c99726c-openwebui
-        //       namespace: openwebui
-        //     spec:
-        //       # will remove this... makes no sense to have this
-        //       replica: 1
-        //       retainPVC: false
-        //
-        //       servicePort: 8080
-        //
-        //       image:
-        //         registry: quay.io
-        //         repository: ibmdpdev/open-webui-spati
-        //         tag: latest
-        //         pullPolicy: Always
-        //
-        //       persistence:
-        //         size: 2Gi
-        //         storageClass: gp3
-        //
-        //       env:
-        //         - name: MOLVIEWER_URL
-        //           value: "moleviewer.open.accelerate.science"
-        //         - name: WEBUI_SECRET_KEY
-        //           value: "idontcarewhatthisisbecuasethisisrequiredforsomereason"
-        // "#;
-        // match serde_json::from_str::<super::OpenWebUI>(yaml_data) {
-        //     Ok(owui) => println!("Successfully deserialized OpenWebUI: {:?}", owui),
-        //     Err(e) => eprintln!("Failed to deserialize OpenWebUI: {}", e),
-        // }
-
+    #[test]
+    fn test_deserialize_owui() {
         let owui = super::Owui {
             spec: super::OpenWebUI {
                 replica: 1,
@@ -154,17 +85,7 @@ mod test {
         // ensure that we can serialize the OpenWebUI struct to JSON
         match serde_json::to_string_pretty(&owui) {
             Ok(json) => println!("Successfully serialized OpenWebUI to JSON:\n{}", json),
-            Err(e) => eprintln!("Failed to serialize OpenWebUI to JSON: {}", e),
+            Err(e) => panic!("Failed to serialize OpenWebUI to JSON: {}", e),
         }
-
-        // rustls::crypto::ring::default_provider()
-        //     .install_default()
-        //     .expect("Cannot install default provider with ring");
-        // crate::kube::init_once().await;
-        //
-        // KubeAPI::new(owui)
-        //     .create(CONFIG.owui_namespace.as_str())
-        //     .await
-        //     .unwrap();
     }
 }
